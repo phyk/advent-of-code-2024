22353ed92765abe65880c796885e35938d655a80
Philipp Peter <20492604+phyk@users.noreply.github.com>
2024-12-09 08:17:11 +0000

    day 8

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -4,7 +4,7 @@ fn parse_input(input: &str) -> Vec<(usize, Vec<usize>)> {
     let lines = input.split("\n");
     let mut equations = vec![];
     for line in lines {
-        if line.len() == 0{
+        if line.len() == 0 {
             continue;
         }
         let mut first_split = line.split(": ");
@@ -34,8 +34,17 @@ fn recursive_apply(
     if current_result > final_result {
         return false;
     }
-    return recursive_apply(final_result, current_result + all_numbers[current_index], current_index + 1, all_numbers)
-        || recursive_apply(final_result, current_result * all_numbers[current_index], current_index + 1, all_numbers);
+    return recursive_apply(
+        final_result,
+        current_result + all_numbers[current_index],
+        current_index + 1,
+        all_numbers,
+    ) || recursive_apply(
+        final_result,
+        current_result * all_numbers[current_index],
+        current_index + 1,
+        all_numbers,
+    );
 }
 
 fn recursive_apply_two(
@@ -57,16 +66,24 @@ fn recursive_apply_two(
     let current_result_str: String = current_result.to_string();
     let number = all_numbers[current_index].to_string();
     let result = usize::from_str_radix(&(current_result_str + &number), 10).unwrap();
-    return recursive_apply_two(final_result, current_result + all_numbers[current_index], current_index + 1, all_numbers)
-        || recursive_apply_two(final_result, current_result * all_numbers[current_index], current_index + 1, all_numbers)
-        || recursive_apply_two(final_result, result, current_index + 1, all_numbers);
+    return recursive_apply_two(
+        final_result,
+        current_result + all_numbers[current_index],
+        current_index + 1,
+        all_numbers,
+    ) || recursive_apply_two(
+        final_result,
+        current_result * all_numbers[current_index],
+        current_index + 1,
+        all_numbers,
+    ) || recursive_apply_two(final_result, result, current_index + 1, all_numbers);
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
     let parsed_input = parse_input(input);
     let mut sum_valid = 0;
     for equation in parsed_input {
-        if recursive_apply(equation.0, equation.1[0], 1, &equation.1){
+        if recursive_apply(equation.0, equation.1[0], 1, &equation.1) {
             sum_valid += equation.0;
         }
     }
@@ -77,7 +94,7 @@ pub fn part_two(input: &str) -> Option<usize> {
     let parsed_input = parse_input(input);
     let mut sum_valid = 0;
     for equation in parsed_input {
-        if recursive_apply_two(equation.0, equation.1[0], 1, &equation.1){
+        if recursive_apply_two(equation.0, equation.1[0], 1, &equation.1) {
             sum_valid += equation.0;
         }
     }
---
+++ src/bin/08.rs
@@ -0,0 +1,99 @@
+advent_of_code::solution!(8);
+
+use std::{collections::HashMap, iter::zip};
+use itertools::Itertools;
+
+use nalgebra::{self, DMatrix};
+
+fn parse_input(input: &str) -> (DMatrix<char>, HashMap<char, Vec<(isize, isize)>>) {
+    let lines: Vec<&str> = input.split("\n").collect();
+    let mut locations = nalgebra::DMatrix::from_element(lines.len() - 1, lines[0].len(), '.');
+    let mut antennas: HashMap<char, Vec<(isize, isize)>> = HashMap::new();
+    for (y, line) in zip(0..lines.len(), lines) {
+        for (idx, char_) in zip(0..line.len(), line.chars()) {
+            locations[(y, idx)] = char_;
+            if char_ != '.' {
+                if !antennas.contains_key(&char_) {
+                    antennas.insert(char_, vec![(y.try_into().unwrap(), idx.try_into().unwrap())]);
+                } else {
+                    antennas.get_mut(&char_).unwrap().push((y.try_into().unwrap(), idx.try_into().unwrap()));
+                }
+            }
+        }
+    }
+    (locations, antennas)
+}
+
+fn in_bounds(pos: (isize, isize), bounds: (usize, usize)) -> bool {
+    pos.0 >= 0 && pos.1 >= 0 && pos.0 < bounds.0.try_into().unwrap() && pos.1 < bounds.1.try_into().unwrap()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (locations, antennas) = parse_input(input);
+    let bounds = (locations.nrows(), locations.ncols());
+    let mut antinodes = nalgebra::DMatrix::from_element(bounds.0, bounds.1, 0);
+    for (_, positions) in antennas {
+        for pos in positions.into_iter().combinations(2) {
+            let a = pos[0];
+            let b = pos[1];
+            let diff = (a.0 - b.0, a.1 - b.1);
+            let check_1 = (a.0 + diff.0, a.1 + diff.1);
+            let check_2 = (b.0 - diff.0, b.1 - diff.1);
+            if in_bounds(check_1, bounds){
+                let convert = (check_1.0.try_into().unwrap(), check_1.1.try_into().unwrap());
+                antinodes[convert] = 1;
+            }
+            if in_bounds(check_2, bounds) {
+                let convert = (check_2.0.try_into().unwrap(), check_2.1.try_into().unwrap());
+                antinodes[convert] = 1;
+            }
+        }
+    }
+    println!("{}", antinodes);
+    Some(antinodes.sum())
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let (locations, antennas) = parse_input(input);
+    let bounds = (locations.nrows(), locations.ncols());
+    let mut antinodes = nalgebra::DMatrix::from_element(bounds.0, bounds.1, 0);
+    for (_, positions) in antennas {
+        for pos in positions.into_iter().combinations(2) {
+            let a = pos[0];
+            let b = pos[1];
+            let diff = (a.0 - b.0, a.1 - b.1);
+            let mut check = (a.0, a.1);
+            while in_bounds(check, bounds) {
+                let convert = (check.0.try_into().unwrap(), check.1.try_into().unwrap());
+                antinodes[convert] = 1;
+                check = (check.0 + diff.0, check.1 + diff.1);
+            }
+            check = (b.0, b.1);
+            while in_bounds(check, bounds) {
+                let convert = (check.0.try_into().unwrap(), check.1.try_into().unwrap());
+                antinodes[convert] = 1;
+                check = (check.0 - diff.0, check.1 - diff.1);
+            }
+            
+        }
+    }
+    println!("{}", antinodes);
+    Some(antinodes.sum())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(14));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(34));
+    }
+}
